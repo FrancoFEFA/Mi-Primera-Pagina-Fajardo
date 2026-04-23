02e3a15b1c7435f2390ec5ffa09c82fc5a8470c9
Franco F <francofefa2014@gmail.com>
2026-04-23 14:36:41 -0300

    Agregado de formulario personas

--- mi_proyecto/mainn/urls.py
+++ mi_proyecto/mainn/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('personas/', views.lista_personas, name='lista_personas'),
+    path('personas/crear/', views.crear_persona, name='crear_persona'),
     # Aquí irán tus rutas, por ejemplo:
     # path('', views.inicio, name='inicio'),
 ]
--- mi_proyecto/mainn/views.py
+++ mi_proyecto/mainn/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from .models import Persona
 
+from .forms import PersonaForm
+
+# Create your views here.
+
 def inicio(request):
     return render(request, 'inicio.html')
 
@@ -10,4 +14,13 @@ def lista_personas(request):
     personas = Persona.objects.all()
     return render(request, 'personas.html', {'personas': personas})
 
-# Create your views here.
+
+def crear_persona(request):
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'persona_creada.html')
+    else:
+        form = PersonaForm()
+    return render(request, 'crear_persona.html', {'form': form})
